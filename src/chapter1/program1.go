f486f76c69ad12365b0c77702f3fa305276c0ec1
Dayalan Saravanan <dayalsaravanan@gmail.com>
2021-06-17 20:44:59 +0530

    program to print a statement

---
+++ src/chapter1/program1.go
@@ -0,0 +1,12 @@
+// File: program1.go
+// Name: D.Saravanan
+// Date: 17/06/2021
+// Program to print a statement
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Institute of Mathematical Sciences")
+}
