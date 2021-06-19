075adef15791efdd43e6824016129d99909ea106
Dayalan Saravanan <dayalsaravanan@gmail.com>
2021-06-19 16:58:04 +0530

    program using numbers

---
+++ src/chapter2/program1.go
@@ -0,0 +1,13 @@
+// File: program1.go
+// Name: D.Saravanan
+// Date: 19/06/2021
+// Program using numbers
+
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("1 + 1 =", 1 + 1)
+    fmt.Println("1 + 1 =", 1.0 + 1.0)
+}
