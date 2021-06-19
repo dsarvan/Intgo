db22a4f0bcc0c54e2e3b464b467082c26b0be328
Dayalan Saravanan <dayalsaravanan@gmail.com>
2021-06-19 19:47:47 +0530

    program using boolean values

---
+++ src/chapter2/program3.go
@@ -0,0 +1,16 @@
+// File: program3.go
+// Name: D.Saravanan
+// Date: 19/06/2021
+// Program using boolean values
+
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println(true && true)
+    fmt.Println(true && false)
+    fmt.Println(true || true)
+    fmt.Println(true || false)
+    fmt.Println(!true)
+}
