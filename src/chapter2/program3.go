3ef31ad8650820613460c6401efaa034b535329d
Dayalan Saravanan <dayalsaravanan@gmail.com>
2021-07-27 20:47:17 +0530

    boolean values logic

--- src/chapter2/program3.go
+++ src/chapter2/program3.go
@@ -8,9 +8,9 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println(true && true)
-    fmt.Println(true && false)
-    fmt.Println(true || true)
-    fmt.Println(true || false)
-    fmt.Println(!true)
+    fmt.Println(true && true)   /* true  */
+    fmt.Println(true && false)  /* false */
+    fmt.Println(true || true)   /* true  */
+    fmt.Println(true || false)  /* true  */
+    fmt.Println(!true)          /* false */
 }
