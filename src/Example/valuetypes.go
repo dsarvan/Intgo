3969fce9b08b4e6f8ddb04a488d5518f8907cc38
Dayalan Saravanan <dayalsaravanan@gmail.com>
2022-02-25 00:23:15 +0530

    basic value types

---
+++ src/Example/valuetypes.go
@@ -0,0 +1,25 @@
+// File: valuetypes.go
+// Name: D.Saravanan
+// Date: 25/02/2022
+// Value types (integer, float, string, boolean)
+
+package main
+
+import "fmt"
+
+func main() {
+
+	// integer
+	fmt.Println("1+1 =", 1+1)
+
+	// float value
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	// string value
+	fmt.Println("go" + "lang")
+
+	// boolean
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
