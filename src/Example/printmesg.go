5a8da41736de19a5309ebc85a0130dcdd343d101
Dayalan Saravanan <dayalsaravanan@gmail.com>
2022-02-25 00:07:53 +0530

    program to print a message

---
+++ src/Example/printmesg.go
@@ -0,0 +1,12 @@
+// File: printmesg.go
+// Name: D.Saravanan
+// Date: 25/02/2022
+// Program to print a message
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("I'd choose you again and again.")
+}
